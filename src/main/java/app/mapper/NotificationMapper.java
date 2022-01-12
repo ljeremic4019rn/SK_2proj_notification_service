3cccfb8a7ce4af9e1cc95ef5e01054c34aa78116
Luka Jeremic <ljeremic4019rn@raf.rs>
2022-01-12 15:02:52 +0100

    random push

--- src/main/java/app/mapper/NotificationMapper.java
+++ src/main/java/app/mapper/NotificationMapper.java
@@ -1,4 +1,35 @@
 package app.mapper;
 
+import app.domain.Notification;
+import app.dto.NotificationCreateDto;
+import app.dto.NotificationDto;
+import org.aspectj.weaver.ast.Not;
+import org.springframework.stereotype.Component;
+
+@Component
 public class NotificationMapper {
+
+    public NotificationMapper() {
+    }
+
+    public NotificationDto notificationToNotificationDto (Notification notification){
+        NotificationDto notificationDto = new NotificationDto();
+        notificationDto.setId(notification.getId());
+        notificationDto.setClientEmail(notification.getClientEmail());
+        notificationDto.setText(notification.getText());
+        notificationDto.setType(notification.getType());
+        notificationDto.setCreationDate(notification.getCreationDate());
+
+        return notificationDto;
+    }
+
+    public Notification notificationCreateDtoToNotification (NotificationCreateDto notificationCreateDto){
+        Notification notification = new Notification();
+        notification.setClientEmail(notificationCreateDto.getClientEmail());
+        notification.setText(notificationCreateDto.getText());
+        notification.setType(notificationCreateDto.getType());
+        notification.setCreationDate(notification.getCreationDate());
+
+        return notification;
+    }
 }
